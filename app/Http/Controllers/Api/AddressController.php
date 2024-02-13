3ac6234e6193baeb8aec1a04a0a45d8255d60a97
Randi <randimaulanaakbar@gmail.com>
2024-02-13 08:22:57 +0700

    feat: update

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Address;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AddressController extends Controller
 {
@@ -12,11 +14,12 @@ class AddressController extends Controller
      */
     public function index(Request $request)
     {
-        $addresses = $request->user()->addresses;
+        $addresses = Address::where('user_id', $request->user()->id)->paginate(5);
+
         return response()->json([
-            'status' => 'success',
+            'status' => 'Success',
             'data' => $addresses
-        ]);
+        ], 200);
     }
 
     /**
@@ -24,7 +27,34 @@ class AddressController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'full_address' => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+            'prov_id' => 'required|string|max:255',
+            'city_id' => 'required|string|max:255',
+            'district_id' => 'required|string|max:255',
+            'village' => 'required|string|max:255',
+            'postal_code' => 'required|string|max:10',
+            'is_default' => 'required|boolean',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $validator->errors()->first()
+            ], 400);
+        }
+
+        $address = new Address();
+        $address->fill($request->all());
+        $address->user_id = $request->user()->id;
+        $address->save();
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => $address
+        ], 201);
     }
 
     /**
@@ -32,7 +62,19 @@ class AddressController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $address = Address::find($id);
+
+        if (!$address) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Address not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => $address
+        ], 200);
     }
 
     /**
@@ -40,7 +82,41 @@ class AddressController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $address = Address::find($id);
+
+        if (!$address) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Address not found'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'full_address' => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+            'prov_id' => 'required|string|max:255',
+            'city_id' => 'required|string|max:255',
+            'district_id' => 'required|string|max:255',
+            'village' => 'required|string|max:255',
+            'postal_code' => 'required|string|max:10',
+            'is_default' => 'required|boolean',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $validator->errors()->first()
+            ], 400);
+        }
+
+        $address->fill($request->all());
+        $address->save();
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => $address
+        ], 200);
     }
 
     /**
@@ -48,6 +124,20 @@ class AddressController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $address = Address::find($id);
+
+        if (!$address) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Address not found'
+            ], 404);
+        }
+
+        $address->delete();
+
+        return response()->json([
+            'status' => 'Success',
+            'message' => 'Address deleted successfully'
+        ], 200);
     }
 }
